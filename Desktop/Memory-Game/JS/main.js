4fd46b529ee69999cb9c4b91b78073ce1444738c
Matt Steffey <Matt@Matts-MacBook-Pro.local>
2017-11-28 15:02:36 -0700

    Memory Game v1

---
+++ Desktop/Memory-Game/JS/main.js
@@ -0,0 +1,62 @@
+var cards = [
+{
+rank: "Queen",
+suit: "Hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "Queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "King",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay = [];
+
+
+var checkForMatch = function () {
+
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+
+  }
+};
+
+
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
+	var card = cards[cardId];
+	this.setAttribute("src", card.cardImage);
+	this.setAttribute("class", "clicked");
+	console.log(this);
+	cardsInPlay.push(card.rank);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+var createBoard = function(){
+	for (var i = 0; i <cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i)
+		cardElement.addEventListener("click", flipCard);
+		var board = document.getElementById("game-board");
+		board.appendChild(cardElement);
+	}
+};
+
+
+createBoard();
